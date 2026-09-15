af2a9d9bf5da54dd601f4432e8dc9fd61e835061
a.vallarino.h@outlook.com <a.vallarino.h@outlook.com>
2026-09-15 07:19:24 +0000

    Muestra de ejecución

--- models/processing/prc_montly_sales_make.sql
+++ models/processing/prc_montly_sales_make.sql
@@ -1,6 +1,8 @@
 {{
     config(
-        materialized= 'table',
+        materialized= 'incremental',
+        incremental_strategy= 'merge',
+        unique_key=['DDATE', 'SALESPERSON', 'CARMAKE'],
         tags= ['processing', 'sales', 'carmake']
     )
 }}
@@ -12,4 +14,17 @@ SELECT
     SUM(SALEPRICE - COMRATE) AS TOTALSALE,
     SUM(COMRATE) AS TOTALCOM
 FROM {{ ref("stg_sales_data") }}
+{% if is_incremental() %}
+    -- Filtra solo los nuevos eventos de la última ejecución
+    -- WHERE event_timestamp > (SELECT MAX(event_timestamp) FROM {{ this }})
+    -- WHERE DDATE > (SELECT MAX(DDATE) FROM {{ this }})
+    -- WHERE event_timestamp :: date >= dateadd(day, -3, current_date())
+    WHERE DDATE :: date >= dateadd(day, -3, current_date())
+{% endif %}
 GROUP BY DATE_TRUNC('MONTH', DDATE), SALESPERSON, CARMAKE
+--        incremental_strategy= 'insert_overwrite',
+--        partition_by= {
+--            "field": "DDATE",
+--            "data_type": "date",
+--            "granularity": "day" 
+--        },
